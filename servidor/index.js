85883d14001ad9b0961849cb8f64caacef7f42ee
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-04-24 18:53:15 -0300

    metd post express

--- servidor/index.js
+++ servidor/index.js
@@ -8,7 +8,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
+app.post('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
